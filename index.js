3efd095b584384d51f3df18f5a05b0bfa2336d3c
shingo45endo <shingo45endo@gmail.com>
2019-05-06 22:12:01 +0900

    Rename main.js to rcm2smf.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Set options as a parameter, environment variable, or rc file.
 require = require('esm')(module/* , options */);	/* globals require:true */
-module.exports = require('./main.js');
+module.exports = require('./rcm2smf.js');
