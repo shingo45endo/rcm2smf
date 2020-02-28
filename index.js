fbe53f572a4b121217358e91d993a1b12aa14e3a
shingo45endo <shingo45endo@gmail.com>
2020-02-29 00:11:44 +0900

    Fix ESLint error

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Set options as a parameter, environment variable, or rc file.
-require = require('esm')(module/* , options */);	/* globals require:true */
+// eslint-disable-next-line no-global-assign
+require = require('esm')(module/* , options */);
 module.exports = require('./rcm2smf.js');
